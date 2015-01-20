7c73604e58fbfdc9635325388fa6bdfe3c1d47b0
marionuhlendorff <marion.uhlendorff@googlemail.com>
2015-01-20 21:44:27 +0100

    session redirect_to '/users/signin'

--- controllers/sessions.rb
+++ controllers/sessions.rb
@@ -4,7 +4,13 @@ module ToDoApp
 			include ToDoApp::Controller
 			action 'Create' do
 				def call(params)
-					puts "hallo"
+					user = ToDoApp::Repositories::UserRepository.find_by_email_and_password(params[:email], params[:password]) #kein .first dahinter, da schon im user_repository
+					if user.nil?
+						#user not found
+						redirect_to '/users/signin'	
+					else
+						#found something
+					end		
 				end
 			end
 		end
